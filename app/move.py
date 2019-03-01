c739e1cf6eb19427f705f36ea3bb54e71aa6ae8f
jonahkoebernick <jojo52jojo52@gmail.com>
2019-03-01 15:47:48 -0800

    Fixed edge case

--- app/move.py
+++ app/move.py
@@ -1,38 +1,50 @@
 
 import numpy as np
+UNOCCUPIED = 0
+OCCUPIED   = 1
+FOOD       = -10
+HEAD       = 1
+
 def calculate_move(board_matrix,head,height):
-    directions = {'up':0, 'down':0, 'left':0, 'right':0}
+    directions = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
     # Check up
-    if head["y"] - 1 < 0 or board_matrix[head["y"] - 1,head["x"]] == 1:
+    if head["y"] - 1 < 0 or board_matrix[head["y"] - 1,head["x"]] == OCCUPIED :
         directions["up"] = 100
     else:
-        directions["up"] = sum(board_matrix, head["x"], head["y"] - 1)
+
+        directions["up"] = sum(board_matrix, head["x"], head["y"] - 1,height)
 
     # Check down
-    if head["y"] + 1 >= (height - 1) or board_matrix[head["y"] + 1 ,head["x"]] == 1:
+    if head["y"] + 1 > (height - 1) or board_matrix[head["y"] + 1 ,head["x"]] == OCCUPIED :
         directions["down"] = 100
     else:
-        directions["down"] = sum(board_matrix, head["x"], head["y"] + 1)
+        directions["down"] = sum(board_matrix, head["x"], head["y"] + 1,height)
 
     # Check Left
-    if head["x"] - 1 < 0 or board_matrix[head["y"] ,head["x"] - 1] == 1:
+    if head["x"] - 1 < 0 or board_matrix[head["y"] ,head["x"] - 1] == OCCUPIED :
         directions["left"] = 100
     else:
-        directions["left"] = sum(board_matrix, head["x"] - 1, head["y"])
+        directions["left"] = sum(board_matrix, head["x"] - 1, head["y"],height)
 
     # check right
-    if head["x"] + 1 >= (height - 1) or board_matrix[head["y"] ,head["x"] + 1] == 1:
+    if head["x"] + 1 > (height - 1) or board_matrix[head["y"] ,head["x"] + 1] == OCCUPIED :
         directions["right"] = 100
     else:
-        directions["right"] = sum(board_matrix, head["x"] + 1, head["y"])
+        directions["right"] = sum(board_matrix, head["x"] + 1, head["y"],height)
 
     return min(directions, key=lambda k: directions[k])
 
-def sum(matrix,x,y):
-    sum =0
-    sum += matrix[y, x-1]
-    sum += matrix[y, x+1]
-    sum += matrix[y-1, x]
-    sum += matrix[y+1, x]
+def sum(matrix,x,y,height):
+    sum = 0
+    if matrix[y,x] == FOOD:
+        return -100
+    if  (x-1) >= 0:
+        sum += matrix[y, x-1]
+    if (x + 1) < height:
+        sum += matrix[y, x+1]
+    if (y-1) >= 0:
+        sum += matrix[y-1, x]
+    if (y+1) < height:
+        sum += matrix[y+1, x]
     return sum
 
