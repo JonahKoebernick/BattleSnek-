0c7301e16722484b2d733854695898e19b87403e
jonahkoebernick <jojo52jojo52@gmail.com>
2019-03-01 13:11:43 -0800

    Added Dumb snake logic

--- app/main.py
+++ app/main.py
@@ -31,9 +31,52 @@ def start():
 @bottle.post('/move')
 def move():
     game_state = bottle.request.json
+    height = game_state["board"]["height"]
+    board_matrix = [[0] * height for i in range(height)]
+    body = game_state["you"]["body"]
+    head = game_state["you"]["body"][0]
+    for i in range(len(body)):
+        board_matrix[body[i]["x"]][body[i]["y"]]=1
+    directions = {'up':0, 'down':0, 'left':0, 'right':0}
 
-    return move_response(direction)
+    #Check up
+    if  head["y"]-1 < 0 or board_matrix[head["x"]][head["y"]-1]==1:
+         directions["up"]=100
+    else:
+        directions["up"] = sum(board_matrix,head["x"],head["y"]-1)
 
+    #Check down
+    if head["y"]+1 >= (height-1)  or board_matrix[head["x"]][head["y"]+1]==1:
+        directions["down"]=100
+    else:
+        directions["down"] = sum(board_matrix, head["x"], head["y"] +1)
+
+    #Check Left
+    if  head["x"]-1 < 0 or board_matrix[head["x"]-1][head["y"]]==1:
+        directions["left"]=100
+    else:
+        directions["left"] = sum(board_matrix, head["x"]-1, head["y"] )
+
+    #check right
+    if head["x"]+1 > (height-1) or  board_matrix[head["x"]+1][head["y"]] == 1:
+        directions["right"]=100
+    else:
+        directions["right"] = sum(board_matrix, head["x"]+1, head["y"] )
+
+
+
+    print(game_state)
+    print(board_matrix)
+
+    return move_response(min(directions, key=lambda k: directions[k]))
+
+def sum(matrix,x,y):
+    sum =0
+    sum += matrix[x-1][y]
+    sum += matrix[x+1][y]
+    sum += matrix[x][y-1]
+    sum += matrix[x][y+1]
+    return sum
 
 @bottle.post('/end')
 def end():
